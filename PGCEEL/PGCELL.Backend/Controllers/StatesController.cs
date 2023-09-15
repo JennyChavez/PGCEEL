8f6ebd8ae80c315499f70917ca67837f85a317cd
JennyChavez <jennychavez1124248@correo.itm.edu.co>
2023-09-15 00:50:13 -0500

    Creation front states and cities

--- PGCEEL/PGCELL.Backend/Controllers/StatesController.cs
+++ PGCEEL/PGCELL.Backend/Controllers/StatesController.cs
@@ -1,15 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PGCEEL.Shared.Entities;
+using PGCELL.Backend.Controllers;
+using PGCELL.Backend.Data;
 using PGCELL.Backend.Intertfaces;
 
-namespace PGCELL.Backend.Controllers
+
+namespace Sales.Backend.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class StatesController : GenericController<State>
     {
-        public StatesController(IGenericUnitOfWork<State> unitOfWork) : base(unitOfWork)
+        private readonly DataContext _context;
+
+        public StatesController(IGenericUnitOfWork<State> unitOfWork, DataContext context) : base(unitOfWork)
         {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public override async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.States
+                .Include(s => s.Cities)
+                .ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public override async Task<IActionResult> GetAsync(int id)
+        {
+            var state = await _context.States
+                .Include(s => s.Cities)
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+            return Ok(state);
         }
     }
 }
+
